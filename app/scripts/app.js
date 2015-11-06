e132b532cc25c0af0cbb50493d6cfe214728583e
Rachel <racheldweissman@gmail.com>
2015-11-05 21:00:10 -0600

    Break added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,10 @@
+//variables for full time, short break, and long break -- global
+
+var workTimer = 1500;
+var shortBreak = 300;
+var longBreak = 1800;
+
+
 var blocTimeModule = angular.module('blocTimeModule', ['ui.router']);
 
 blocTimeModule.config(function($stateProvider, $urlRouterProvider, $locationProvider){
@@ -52,26 +59,60 @@ blocTimeModule.controller("CountdownTimerController", [
   '$interval',
   function($log, $scope, $interval) {
     $log.debug("CountdownTimerController");
-    $scope.counter = 1500;
-    var stop;
-    var isTimerRunning = false;
+    // $scope.counter = 1500;
+    // var stop;
+    // var isTimerRunning = false;
+    $scope.isTimerRunning = false;
+    $scope.breakTime = false;
+    var pomodoros = 0;
+    var pomodoroGo;
 
     $scope.startTimer = function () {
-      this.isTimerRunning = true;
-      stop = $interval(function(){
-        $scope.counter--;
-        if($scope.counter == 0) {
-          $interval.cancel(stop);
-          $scope.counter = 1500;
-        }
-      }, 1000);
+          $scope.isTimerRunning = true;
+
+          if (!$scope.counter) {
+            $scope.counter = workTimer;
+          }
+
+          pomodorGo = $interval(function() {
+            $scope.counter--;
+            if ($scope.counter == 0) {
+              $interval.cancel(pomodorGo);
+              $scope.isTimerRunning = false;
+
+              if (!$scope.breakTime) {
+                pomodoros++;
+                console.log(pomodoros);
+                $scope.breakTime = true;
+
+                if (pomodoros % 4 === 0) {
+                  $scope.counter = longBreak;
+                  $scope.isTimerRunning = false;
+                } else {
+                  $scope.counter = shortBreak;
+                  $scope.isTimerRunning = false;
+                }
+              } else {
+                console.log("back to work");
+                $scope.breakTime = false;
+                $scope.counter = workTimer;
+              }
+            }
+          }, 1000);
     };
 
-    $scope.stopTimer = function() {
-      $interval.cancel(stop);
-      $scope.counter = 1500;
-      this.isTimerRunning = false;
-    }
+    $scope.resetTimer = function() {
+    $interval.cancel(pomodorGo);
+    $scope.counter = workTimer;
+    $scope.isTimerRunning = false;
+  }
+
+
+    // $scope.stopTimer = function() {
+    //   $interval.cancel(stop);
+    //   $scope.counter = 1500;
+    //   this.isTimerRunning = false;
+    // }
 
     // $scope.timeRemaining = 100;
 
